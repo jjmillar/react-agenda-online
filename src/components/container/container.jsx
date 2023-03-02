ba90f33474453952af2631d09f51696b242b911e
Julian Millar <julianjmillar@gmail.com>
2023-03-02 16:43:41 -0300

    avance

---
+++ src/components/container/container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Container = () => {
+    return(
+        <div className="container">
+            <p>hello world</p>
+        </div>
+    )
+};
+
+export default Container;
