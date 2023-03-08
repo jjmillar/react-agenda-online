50651476911c92a2651acaf28cd29757faea67d4
Julian Millar <julianjmillar@gmail.com>
2023-03-08 10:28:26 -0300

    fixed home import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/nav/Navbar";
-import Slider from "../components/slider/slider";
+import Slider from "../components/slider/Slider";
 
 const Home = () => {
   return (
