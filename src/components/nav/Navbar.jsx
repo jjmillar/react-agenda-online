5966a4ee1d1c0c3b099bb2b76153b61229fa39da
Julian Millar <julianjmillar@gmail.com>
2023-03-08 21:36:15 -0300

    added icon and responsive menu

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -6,17 +6,20 @@ const Navbar = () => {
   return (
     <nav className={classes.navBar}>
       <div className={classes.logo}>
-      <img src="https://tinypic.host/images/2023/03/07/logo9e41e4e8bc630240.png" alt="logo9e41e4e8bc630240.png" border="0" />
+      <img src="./src/assets/logo/logo.svg" alt="logo estetica girardi"/>
       </div>
 
       <div className={classes.name}>
-        <p>Estetica Girardi</p>
+        <p>Estética Girardi</p>
       </div>
 
       <div className={classes.ico}>
         <a href="https://www.instagram.com/estetica_girardi/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
         <a href="https://wa.me/56962105309" target="_blank"><i className="fa-brands fa-whatsapp"></i></a>
         <a href="mailto:julianjmillar@gmail.com"><i className="fa-regular fa-envelope"></i></a>
+      </div>
+      <div className={classes.menu}>
+      <ion-icon name="menu-outline"></ion-icon>
       </div>
     </nav>
   );
