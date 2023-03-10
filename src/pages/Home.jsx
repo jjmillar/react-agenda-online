3224d5ac10470fb6f84b889a89cc41930b5be914
Julian Millar <julianjmillar@gmail.com>
2023-03-10 12:56:21 -0300

    articles and scroll snap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import Navbar from "../components/nav/Navbar";
 import Slider from "../components/slider/Slider";
 import SlideDown from "../components/slide-down/slideDown";
+import TreatmentCard from "../components/treatment/treatment";
+import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <Slider />
-      <SlideDown/>
+      <div className="articles">
+        <article className="page">
+          <Slider />
+          <SlideDown />
+        </article>
+        <article className="page">
+          <TreatmentCard />
+        </article>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Home;
