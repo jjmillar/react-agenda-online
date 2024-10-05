06d53730421a630d86708e320e5bf5919ff34d29
Julián Millar <julianjmillar@gmail.com>
2024-10-05 11:30:19 -0300

    shore: alctualized insta link

--- src/components/hamburger-menu/menu/menu.jsx
+++ src/components/hamburger-menu/menu/menu.jsx
@@ -4,7 +4,7 @@ import "./menu.scss";
 
 const Menu = ({ close }) => (
   <div className="menu">
-    <a href="https://www.instagram.com/estetica_girardi/" target="_blank">
+    <a href="https://www.instagram.com/girardiclinica/" target="_blank">
       <i className="fa-brands fa-instagram popico"></i> INSTAGRAM
     </a>
     <a href="https://wa.me/56962105309" target="_blank">
